a7cf18470bd58072181eb5ffb04424edcd9a2d0b
AApsley <andy.apsley@googlemail.com>
2021-05-19 07:26:26 +0000

    changes made to interest app by aa

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function compute()
 {
+    
+
+
     p = document.getElementById("principal").value;
+
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-    /*Updates year*/
+    
     var year = new Date().getFullYear()+parseInt(years); 
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%.\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
 }
